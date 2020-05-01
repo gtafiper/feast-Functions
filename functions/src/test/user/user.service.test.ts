10076d2c66a1787bc9493836f275adba5c55afc8
nijashansen <nijas.w.hansen@gmail.com>
2020-05-01 11:20:04 +0200

    delete func tests
    2 test på delete functions

---
+++ functions/src/test/user/user.service.test.ts
@@ -0,0 +1,32 @@
+import {UserRepository} from "../../users/user.repository";
+import {UserService} from "../../users/user.service";
+import {IMock, Mock, Times} from "moq.ts";
+import {User} from "../../models/user";
+
+describe('UserService', () => {
+
+  let userRepository: IMock<UserRepository>;
+  let userService: UserService;
+
+  const user: User = {uid: 'special', name: 'HP', email: 'jenshansen@gmail.com'};
+
+  beforeEach(() => {
+    userRepository = new Mock<UserRepository>()
+      .setup(instance => instance.deleteUser(user.uid))
+      .returns(Promise.resolve())
+
+    userService = new UserService(userRepository.object());
+  });
+
+  it('should check if user repo has a delete function', async () => {
+    await userService.deleteUser(user.uid);
+    userRepository.verify(userRepo => userRepo.deleteUser(user.uid), Times.Exactly(1))
+  });
+
+  it('should delete the user and check if the user is undefined', async () => {
+    // @ts-ignore
+    const userAfter: User = await userService.deleteUser(user);
+    expect(userAfter).toBeUndefined();
+  });
+
+});
