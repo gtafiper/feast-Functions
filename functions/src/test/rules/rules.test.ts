301ad4180a292bcbedd0528adc7fc7f821819534
nijashansen <nijas.w.hansen@gmail.com>
2020-05-07 19:21:39 +0200

    en lille test som skal rettes

--- functions/src/test/rules/rules.test.ts
+++ functions/src/test/rules/rules.test.ts
@@ -7,7 +7,16 @@ describe('Rules testing', () => {
 
   test('Should deny a read to the recipe collection if not logged id', async () => {
     const db = await setup();
+    const recipesRef = db.collection('Recipe')
+    await expect(recipesRef.get()).toDeny();
+  })
+
+  test('should Allow writing to the recipe collection if the user has the role Standard', async () => {
+    const db = await setup({
+      uid: 'isAUser',
+      role: 'Admin'
+    });
     const recipeRef = db.collection('Recipe')
-    await expect(recipeRef.get()).toDeny();
+    await expect(recipeRef.add({data: 'something'})).toAllow();
   })
 })
