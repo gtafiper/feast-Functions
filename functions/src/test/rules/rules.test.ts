c05d16e0cd8c63ef824d2e5c78b9381ac1f2abda
nijashansen <nijas.w.hansen@gmail.com>
2020-05-08 09:56:58 +0200

    test of rules

--- functions/src/test/rules/rules.test.ts
+++ functions/src/test/rules/rules.test.ts
@@ -5,18 +5,46 @@ describe('Rules testing', () => {
     await teardown();
   })
 
+  afterAll(() => {
+    teardown();
+  })
+
   test('Should deny a read to the recipe collection if not logged id', async () => {
     const db = await setup();
-    const recipesRef = db.collection('Recipe')
+    const recipesRef = db.collection('Recipes')
     await expect(recipesRef.get()).toDeny();
   })
 
-  test('should Allow writing to the recipe collection if the user has the role Standard', async () => {
+  test('should deny writing to the recipe collection if the user has the role Standard', async () => {
     const db = await setup({
       uid: 'isAUser',
+      role: 'Standard'
+    });
+    const recipeRef = db.collection('Recipes')
+    await expect(recipeRef.add({data: 'something'})).toDeny();
+  })
+
+  test('Should deny reading from userRecipes by default', async () => {
+    const db = await setup();
+    const userRecipeRef = db.collection('UserRecipe')
+    await expect(userRecipeRef.get()).toDeny();
+  })
+
+  test('Should deny writing to userRecipes by default', async () => {
+    const db = await setup();
+    const userRecipeRef = db.collection('UserRecipe');
+    await expect(userRecipeRef.add({data: 'hullu bullu'})).toDeny();
+  })
+
+  test('should allow write from the users collection if the user is admin', async () => {
+    const db = await setup( {uid: '5lzBYBYT1yQmagbOYzIpTRU0WQ62'}, {
+      uid: '5lzBYBYT1yQmagbOYzIpTRU0WQ62',
+      email: 'ksdhjfkshdf',
+      userRecipe: [],
       role: 'Admin'
     });
-    const recipeRef = db.collection('Recipe')
-    await expect(recipeRef.add({data: 'something'})).toAllow();
+    const userRef = db.collection('Users');
+    console.log(userRef);
+    await expect(userRef.add({data: 'hullu bullu'})).toAllow();
   })
 })
